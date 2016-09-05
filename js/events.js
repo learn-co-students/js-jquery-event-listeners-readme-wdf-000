8b9223bb02192c8d05cb4ce82323e893ded90419
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-09-05 17:29:15 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,44 @@
 //define functions here
 
+function getIt() {
+  // $('p').on("click",function(){
+  //   alert("Hey!");
+  //   return;
+  // });
+  $('p').click(function () {
+    alert("Hey!");
+    return;
+  });
+}
+
+function frameIt() {
+  $('img').load(function (e) {
+    $('img').addClass("tasty");
+  });
+  return;
+}
+
+function pressIt() {
+  $(document).keydown(function (e) {
+    if (e.which == 71) {
+      alert('g was pressed');
+    }
+  });
+  return;
+}
+
+function submitIt() {
+  $('form').submit(function (e) {
+    alert("Your form is going to be submitted now.");
+  });
+  return;
+}
+
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
