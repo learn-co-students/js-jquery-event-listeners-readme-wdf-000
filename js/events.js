093f13ff283021aa5c91f74c312275359ee4fe18
Margaret <eutopia@gmail.com>
2016-09-27 13:00:33 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
-//define functions here
+function getIt() {
+  $('p').click(function () {
+    alert("Click!");
+    return;
+  });
+}
+
+function frameIt() {
+  $('img').load(function (e) {
+    $('img').addClass("tasty");
+  });
+  return;
+}
+
+function pressIt() {
+  $(document).keydown(function (e) {
+    if ((e.which || e.detail) == 71) {
+      alert('g was pressed!');
+    }
+  });
+  return;
+}
+
+function submitIt() {
+  $('form').submit(function (e) {
+    alert("Your form is going to be submitted now.");
+  });
+  return;
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+	getIt();
+	frameIt();
+	pressIt();
+	submitIt();
 });
