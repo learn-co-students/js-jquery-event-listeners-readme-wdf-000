56b74d49f2e180b972b6c6a65ac85d26b3dd85f0
Derick Quartey <niikwartey1@gmail.com>
2016-09-28 14:50:23 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(e) { alert("Hey!") })
+}
+
+function frameIt(){
+  $('img').on('load', function(e) { $('img').addClass("tasty")})
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if (key.which === 71){ alert('you presses g eh!');}
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
 
 $(document).ready(function(){
 
 // call functions here
+getIt()
+frameIt()
+pressIt()
+submitIt()
 
 });
