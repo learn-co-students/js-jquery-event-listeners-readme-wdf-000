64b70015cd4b537ca9faa7f68bdcf5dbe1331512
satub <satu.barnhill@gmail.com>
2016-10-03 17:56:44 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,35 @@
+function getIt(){
+  $('p').on('click', function (){
+    alert("Hey!")
+  });
+}
+
+function frameIt(){
+  $(document).on('load', function(){
+    $('img').attr('class', 'tasty');
+  });
+}
+
+function pressIt(){
+  $('input#typing').on('keydown', function(key){
+    if (key.which === 71) {
+      alert("You pressed g!");
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on("submit", function() {
+    alert('Your form is going to be submitted now.');
+  })
+}
 //define functions here
 
+
+
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
