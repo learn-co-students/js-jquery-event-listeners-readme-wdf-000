fae4a81bc3ba7a2e1bf7a0937e4d368565bcb147
ozPop <ozziepopovas@gmail.com>
2016-10-18 09:01:19 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,32 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert( $(this).text() );
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img:first').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert();
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
+  mocha.run();
 
 // call functions here
 
