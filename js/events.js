22554c588e45e94d5e8ca4d12c9dfb090d035667
Isuru <github email address>
2016-11-04 17:09:58 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt(){
+  $('p').on("click",function(){
+      alert("clicked");
+  })
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+     $('img').attr('class','tasty');
+  })
+}
+
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if(key.which == 71){
+      alert('g was pressed');
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit',function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
+
+// $('div#image').
+// prepend('<img id="theImg" src="https://s3.amazonaws.com/learn-verified/painting-with-dog.gif" />')
