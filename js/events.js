219bbce4472fb8c71d0330c23de13778cf8ea634
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-12-06 10:46:56 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
-//define functions here
+//define functions here\
+function getIt(){
+  $("p").on('click', alertHey)
+}
+
+function alertHey(){
+  alert("Hey!");
+}
+
+function frameIt(){
+  $("img").on('load', function(){
+    $(this).addClass("tasty");
+  })
+}
+
+function pressIt(){
+  $("input#typing").on('keydown', function(key) {
+    if(key.which == 71){
+      alert();
+  }
+  });
+}
+
+function submitIt(){
+  $("form").on("submit", function() {
+
+  alert("Your form is going to be submitted now.");
+
+  });
+}
 
 $(document).ready(function(){
-
+ frameIt();
+ pressIt();
 // call functions here
 
 });
